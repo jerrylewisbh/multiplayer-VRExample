8d636f29fe99c13e37da8927f92d6a02cea7a4b7
Jerry <jerrylewisbh@gmail.com>
2020-05-15 18:41:51 -0300

    initial project

---
+++ Assets/Script/ConnectionHandler.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConnectionHandler : MonoBehaviourPunCallbacks
+{
+
+    public GameObject VRRig;
+    public float charactersOffset = 2f;
+
+    public bool connectOnStart = true;
+    
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (connectOnStart)
+        {
+            Connect();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Connecting");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected");
+        Debug.Log("Am I the master: " + PhotonNetwork.IsMasterClient);
+
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 10;
+        PhotonNetwork.JoinOrCreateRoom("Room1", options, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("On Room Joined");
+        PhotonNetwork.LoadLevel("Arena");     
+      
+
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+
+        if (scene.name == "Arena")
+        {
+            var numPlayers = PhotonNetwork.CurrentRoom.PlayerCount;
+            PhotonNetwork.Instantiate(VRRig.name, new Vector3(0,1.7f,numPlayers * charactersOffset), Quaternion.identity);
+        }
+        
+    }
+}
---
+++ Assets/Script/NetworkPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SpatialTracking;
+
+public class NetworkPlayer : MonoBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+
+    private PhotonView view;
+
+    private void Awake()
+    {
+        view = GetComponent<PhotonView>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (view.IsMine)
+        {
+        }
+        else
+        {
+
+            foreach (Behaviour component in componentsToDisable)
+            {
+                component.enabled = false;
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
